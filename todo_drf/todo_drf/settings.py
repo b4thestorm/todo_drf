5b7081d3f77bf980b7bfc76e6c92a1146c605945
arnoldsanders <arnoldsander@gmail.com>
2023-05-07 09:44:18 -0400

    shhh

--- todo_drf/todo_drf/settings.py
+++ todo_drf/todo_drf/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-lc0b+t-gxjp#bvgws-dp5*zok=jte0!+wps075zf4yzho=7a=o"
-
+SECRET_KEY = getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "todo"
 ]
 
 MIDDLEWARE = [
